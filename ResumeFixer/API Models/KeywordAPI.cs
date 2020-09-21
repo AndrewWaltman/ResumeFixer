566d2291b59b920e1977da79be440c0152143dc5
Andrew Waltman <andrew.w.waltman@gmail.com>
2020-09-21 17:15:20 -0400

    first commit, also trying to add keyword API

---
+++ ResumeFixer/API Models/KeywordAPI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResumeFixer.API_Models
+{
+    public class KeywordAPI
+    {
+        public class Rootobject
+        {
+            public string message { get; set; }
+            public string[] result { get; set; }
+            public string status { get; set; }
+        }
+
+    }
+}
